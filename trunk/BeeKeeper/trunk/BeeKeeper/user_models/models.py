09393a973879cda0b813c2b86633b3f7916631a4
romenrg <romenrg@09e8946b-dfe9-8220-69a3-a60357482830>
2011-02-10 19:07:38 +0000

    Aplicación db_models: Definiendo modelos del negocio Aplicación user_models: Definiendo modelos de gestión de usuarios

---
+++ trunk/BeeKeeper/trunk/BeeKeeper/user_models/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=128)
+    surname = models.CharField(max_length=128)
+    
+    def __unicode__(self):
+        return self.name
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(Person, through='Membership')
+
+    def __unicode__(self):
+        return self.name
