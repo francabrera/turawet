16753290283d93eec155082687a7918be8cdc406
nicopernas <nicopernas@09e8946b-dfe9-8220-69a3-a60357482830>
2011-02-17 20:06:49 +0000

    Creo la aplicación Seed, en donde irá el modelador. Tiene trozos del cliente WS pero no hace nada

---
+++ Seed/trunk/Seed/ws_client/views.py
@@ -0,0 +1,26 @@
+# Create your views here.
+
+    
+from suds import WebFault
+from suds.client import Client
+
+    #import logging
+    #from suds.xsd.doctor import Import, ImportDoctor
+    
+    #logging.basicConfig(level=logging.INFO)
+    ##logging.getLogger('suds.client').setLevel(logging.DEBUG)
+
+class WsClient():
+    
+    
+    def __init__(self):
+    
+        self.url = 'http://localhost:8000/ws_server/service.wsdl'
+        self.client = Client(self.url, cache=None)
+        print self.client.service.__find('get_file')
+        
+    #imp = Import('http://schemas.xmlsoap.org/soap/encoding/')
+    #imp.filter.add('http://localhost:8000/soap/') 
+    #doctor = ImportDoctor(imp) 
+    #client = Client(url, doctor=doctor, cache=None)
+    
