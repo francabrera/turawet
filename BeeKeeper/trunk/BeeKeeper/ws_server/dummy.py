53744e2985cb16aff45625c043315d379035d197
nicopernas <nicopernas@09e8946b-dfe9-8220-69a3-a60357482830>
2011-02-17 20:05:15 +0000

    Los métodos etán comentados, a falta de tener los modelos

--- BeeKeeper/trunk/BeeKeeper/ws_server/dummy.py
+++ BeeKeeper/trunk/BeeKeeper/ws_server/dummy.py
@@ -6,7 +6,7 @@ Created on 08/02/2011
 
 from soaplib.serializers.clazz import ClassSerializer
 from soaplib.serializers.primitive import String, Integer
-from models_serializer import ModelsSerializer
+from models_ws import ModelsSerializer
 from soaplib.serializers.primitive import *
 
 #class Dummy(ClassSerializer):
@@ -14,14 +14,14 @@ from soaplib.serializers.primitive import *
 #       entero = Integer
 #        value = String
 
-class Dummy():       
+class Dummy():
     def __init__(self, value, ent):
         self.value = value
         self.entero = ent
-                
 
 
 class DummyWs(ModelsSerializer):
     class types:
         entero = Integer
+        value = String
 
