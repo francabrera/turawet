a0ae61e1d3f477fe3cb1b470b2ca8e19148dd9f0
franxu <franxu@09e8946b-dfe9-8220-69a3-a60357482830>
2011-03-02 18:43:40 +0000

    Agregando modelador en fase inicial

--- Seed/trunk/Seed/modeler/urls.py
+++ Seed/trunk/Seed/modeler/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('modeler.views',
-    (r'modeler', 'showModeler'),
+
+urlpatterns = patterns('',
+    (r'^css/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.CSS_MODELER_ROOT, 'show_indexes': False}),
+    (r'^images/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.IMAGES_MODELER_ROOT, 'show_indexes': False}),
+    (r'^js/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.JS_MODELER_ROOT, 'show_indexes': False}),
+)
+
+urlpatterns += patterns('modeler.views',
+    (r'^$', 'showModeler'),
 )
