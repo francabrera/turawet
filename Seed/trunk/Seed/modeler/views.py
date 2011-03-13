48481fa8cfb1412aa9b49d84350d720aeeb6189f
franxu <franxu@09e8946b-dfe9-8220-69a3-a60357482830>
2011-03-13 23:40:48 +0000

    Pruebas con formulario para el modelador

--- Seed/trunk/Seed/modeler/views.py
+++ Seed/trunk/Seed/modeler/views.py
@@ -3,8 +3,19 @@ Views for Seed web modeler
 Project: Turawet
 Date: 01-03-2011
 """
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from Seed.modeler.forms import NewForm
 
 def showModeler(request):
-    return render_to_response('create.html', RequestContext(request))
+    if request.method == 'POST':
+        form = NewForm(request.POST)
+        if form.is_valid() :
+            return HttpResponseRedirect('/formList')
+    else:
+        form = NewForm()
+        
+    context = RequestContext(request)
+    context['form'] = form
+    return render_to_response('create.html', context)
