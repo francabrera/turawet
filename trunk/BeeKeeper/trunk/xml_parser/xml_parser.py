108f49711759156d1e8488f44e6d3244e6ae5052
romenrg <romenrg@09e8946b-dfe9-8220-69a3-a60357482830>
2011-03-17 20:01:56 +0000

    BeeKeeper: Utilizando ElementTree para realizar el Parsing del XML recibido por el Webservice desde el Modelador. Atributos de "form". Empezando con secciones.

--- trunk/BeeKeeper/trunk/xml_parser/xml_parser.py
+++ trunk/BeeKeeper/trunk/xml_parser/xml_parser.py
@@ -3,3 +3,26 @@
             Nicolas Pernas Maradei,
             Romen Rodriguez Gil
    :version: 0.2"""
+
+from xml.etree.ElementTree import ElementTree;
+
+def generateModels(xml):
+    """
+    """
+    if xml is None:
+        return None
+    else:
+        parser = ElementTree();
+        parser.parse(xml);
+        """Comenzamos con el parser"""
+        id = parser.findtext('id');
+        version = parser.findtext('version');
+        name = parser.findtext('name');
+        user = parser.findtext('author/user');
+        
+        """Section"""
+        sections = parser.findall("section");
+        for section in sections:
+            section.findtext("id");
+            section.findtext("name");
+        
