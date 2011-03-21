90bc40c8d520bd14552f9ba6c5fb0b350c370eb9
romenrg <romenrg@09e8946b-dfe9-8220-69a3-a60357482830>
2011-03-21 19:52:34 +0000

    BeeKeeper: Continuando con el Parser. Parseando los diferentes campos de una sección

--- trunk/BeeKeeper/trunk/xml_parser/xml_parser.py
+++ trunk/BeeKeeper/trunk/xml_parser/xml_parser.py
@@ -6,6 +6,64 @@
 
 from xml.etree.ElementTree import ElementTree;
 
+
+def parseTextField(parser):
+    id = parser.findtext('id');
+    label = parser.findtext('label');
+    maxlength = parser.findtext('maxlength');
+    required = parser.findtext('required');
+    value = parser.findtext('value');
+
+
+def parseDateField(parser):
+    id = parser.findtext('id');
+    label = parser.findtext('label');
+    required = parser.findtext('required');
+    day = parser.findtext('date_value/day');
+    day = parser.findtext('date_value/month');
+    day = parser.findtext('date_value/year');
+
+
+def parseComboField(parser):
+    id = parser.findtext('id');
+    label = parser.findtext('label');
+    required = parser.findtext('required');
+    
+    options = parser.findall("option");
+    for option in options:
+        optionLabel = option.findtext("label");
+        optionValue = option.findtext("value");
+        optionSelected = option.findtext("selected");
+
+
+                <id>id</id>
+                <label>Estudios</label>
+                <required>true</required>
+                <options>
+                    <option>
+                        <label>Ingeniería Informática</label>
+                        <value>IM</value>
+                        <selected>selected</selected>
+                    </option>
+                    <option>
+                        <label>Medicina</label>
+                        <value>M</value>
+                    </option>
+                    <option>
+                        <label>Farmacia</label>
+                        <value>F</value>
+                    </option>
+                </options>
+
+
+""" Data types """
+actionSwitch = {
+    'text': parseTextField,
+    'date': parseDateField,
+    'radio': parseRadioField,
+    'combo': parseComboField
+    }
+
 def generateModels(xml):
     """
     """
@@ -25,4 +83,10 @@ def generateModels(xml):
         for section in sections:
             section.findtext("id");
             section.findtext("name");
+            """Extraemos los campos dentro de la sección"""
+            for field in section:
+                actionSwitch[field.tag](field);
+                
+                
+
         
