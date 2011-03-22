40a742f6c2ffab2f8eab4db452065b90d8c68ff5
romenrg <romenrg@09e8946b-dfe9-8220-69a3-a60357482830>
2011-03-22 18:16:49 +0000

    BeeKeeper: Continuando con el parser. Comenzando a crear objetos de los modelos para la persistencia.

--- trunk/BeeKeeper/trunk/xml_parser/xml_parser.py
+++ trunk/BeeKeeper/trunk/xml_parser/xml_parser.py
@@ -4,59 +4,68 @@
             Romen Rodriguez Gil
    :version: 0.2"""
 
-from xml.etree.ElementTree import ElementTree;
+from xml.etree.ElementTree import ElementTree
+from BeeKeeper.db_models.models import Section, Form
 
 
 def parseTextField(parser):
-    id = parser.findtext('id');
-    label = parser.findtext('label');
-    maxlength = parser.findtext('maxlength');
-    required = parser.findtext('required');
-    value = parser.findtext('value');
+    id = parser.find('id')
+    label = parser.findtext('label')
+    maxlength = parser.findtext('maxlength')
+    required = parser.findtext('required')
+    value = parser.findtext('value')
 
 
 def parseDateField(parser):
-    id = parser.findtext('id');
-    label = parser.findtext('label');
-    required = parser.findtext('required');
-    day = parser.findtext('date_value/day');
-    day = parser.findtext('date_value/month');
-    day = parser.findtext('date_value/year');
+    id = parser.find('id')
+    label = parser.findtext('label')
+    required = parser.findtext('required')
+    day = parser.findtext('date_value/day')
+    day = parser.findtext('date_value/month')
+    day = parser.findtext('date_value/year')
 
 
-def parseComboField(parser):
-    id = parser.findtext('id');
-    label = parser.findtext('label');
-    required = parser.findtext('required');
+def parseRadioField(parser):
+    id = parser.find('id')
+    label = parser.findtext('label')
+    required = parser.findtext('required')
     
-    options = parser.findall("option");
+    options = parser.findall("option")
     for option in options:
-        optionLabel = option.findtext("label");
-        optionValue = option.findtext("value");
-        optionSelected = option.findtext("selected");
-
-
+        optionLabel = option.findtext("label")
+        optionValue = option.findtext("value")
+        optionSelected = option.findtext("selected")
+            <radio><!-- Elementos tipo radio-button -->
                 <id>id</id>
-                <label>Estudios</label>
-                <required>true</required>
+                <label>Sexo</label>
+                <required>false</required>
                 <options>
                     <option>
-                        <label>Ingeniería Informática</label>
-                        <value>IM</value>
-                        <selected>selected</selected>
-                    </option>
-                    <option>
-                        <label>Medicina</label>
+                        <label>Masculino</label>
                         <value>M</value>
+                        <selected/>
                     </option>
                     <option>
-                        <label>Farmacia</label>
+                        <label>Femenino</label>
                         <value>F</value>
                     </option>
                 </options>
+            </radio>
 
 
-""" Data types """
+def parseComboField(parser):
+    id = parser.find('id')
+    label = parser.findtext('label')
+    required = parser.findtext('required')
+    
+    options = parser.findall("option")
+    for option in options:
+        optionLabel = option.findtext("label")
+        optionValue = option.findtext("value")
+        optionSelected = option.findtext("selected")
+
+
+## Data types ##
 actionSwitch = {
     'text': parseTextField,
     'date': parseDateField,
@@ -70,23 +79,28 @@ def generateModels(xml):
     if xml is None:
         return None
     else:
-        parser = ElementTree();
-        parser.parse(xml);
-        """Comenzamos con el parser"""
-        id = parser.findtext('id');
-        version = parser.findtext('version');
-        name = parser.findtext('name');
-        user = parser.findtext('author/user');
+        parser = ElementTree()
+        parser.parse(xml)
+        #Starting the parsing
+        id = parser.find('id')
+        version = parser.findtext('version')
+        name = parser.findtext('name')
+        user = parser.findtext('author/user')
+        # Form model
+        form = Form(version=version, name=name)
+        form.save()
+        id.text = form.id
         
-        """Section"""
-        sections = parser.findall("section");
+        #Section
+        sections = parser.findall("section")
         for section in sections:
-            section.findtext("id");
-            section.findtext("name");
-            """Extraemos los campos dentro de la sección"""
+            id = section.find("id")
+            name = section.findtext("name")
+            section_model = Section(name=name, order="TO-DO")
+            #Section fields
             for field in section:
-                actionSwitch[field.tag](field);
-                
-                
-
-        
+                actionSwitch[field.tag](field)
+            
+        # Saving the XML in the form table
+        form.xml = tostring(parser.getroot)
+        form.save()
