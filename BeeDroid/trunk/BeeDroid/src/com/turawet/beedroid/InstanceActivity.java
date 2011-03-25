82003b6af0a71252aa9f31cc602e99742fab4aea
nicopernas <nicopernas@09e8946b-dfe9-8220-69a3-a60357482830>
2011-03-25 19:03:03 +0000

    Primera aproximación a la aplicación android

---
+++ BeeDroid/trunk/BeeDroid/src/com/turawet/beedroid/InstanceActivity.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.turawet.beedroid;
+
+import android.app.Activity;
+
+/**
+ * @author nicopernas
+ * 
+ */
+public class InstanceActivity extends Activity
+{
+	/**
+	 *
+	 */
+}
