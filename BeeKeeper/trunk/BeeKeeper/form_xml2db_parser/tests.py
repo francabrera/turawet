c528de6a7b1fd2615b3459869e1b27797bdd8405
romenrg <romenrg@09e8946b-dfe9-8220-69a3-a60357482830>
2011-04-11 19:05:08 +0000

    BeeKeeper: Corrigiendo modelos y comenzando test de 'Modelos' y 'Parser 1'

--- BeeKeeper/trunk/BeeKeeper/form_xml2db_parser/tests.py
+++ BeeKeeper/trunk/BeeKeeper/form_xml2db_parser/tests.py
@@ -1,23 +1,29 @@
 """
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
+    Tests unitarios para el parser y los Modelos de la BBDD
 """
-
+#from django.test import TestCase
 from django.test import TestCase
+#import logging
+    #from suds.xsd.doctor import Import, ImportDoctor
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+#from suds.client import Client
+from BeeKeeper.db_models.models import *
+from BeeKeeper.form_xml2db_parser.form_xml2db_parser import FormXmldbParser 
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+import os.path
 
->>> 1 + 1 == 2
-True
-"""}
+class FormXmldbParserTest(TestCase):
+
+    def xml_2_db(self):
+        #a = ['larry', 'jose']
+        #self.assertEqual(my_func(a, 0), 'larry')
+        
+        # XML Sample
+        xml = os.path.dirname(__file__) + 'resources/formulario.xml'
+        # Creationg a parser
+        self.parser = FormXmldbParser()
+        self.parser.generateModels(xml)
+        # All the forms in the DB
+        forms = Form.objects.all()
+        # La base de datos que se levanta para Test es de mentira
 
