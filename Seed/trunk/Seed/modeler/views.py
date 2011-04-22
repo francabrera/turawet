8ea9344ff9f507ceda9aff615a6e209403f0ec13
franxu <franxu@09e8946b-dfe9-8220-69a3-a60357482830>
2011-04-22 14:49:27 +0000

    Cambios en la interfaz del modelador Web

--- Seed/trunk/Seed/modeler/views.py
+++ Seed/trunk/Seed/modeler/views.py
@@ -8,12 +8,15 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from Seed.modeler.forms import NewForm
 
+from xml.etree.ElementTree import ElementTree, tostring, XML
+
 def showModeler(request):
     context = RequestContext(request)
     if request.method == 'POST':
         form = NewForm(request.POST)
         if form.is_valid() :
             context['prueba'] = form.cleaned_data['fieldList']
+            context['prueba2'] = parseSentFormXML
 #        if form.is_valid() :
 #            return HttpResponseRedirect('/formList')
     else:
@@ -21,3 +24,13 @@ def showModeler(request):
         
     context['form'] = form
     return render_to_response('create.html', context)
+
+def parseSentFormXML (xml):
+    if xml is None:
+        return None
+    else:
+        formXML = XML(xml)
+        sections = formXML.findall("section")
+        return tostring(sections);
+
+
