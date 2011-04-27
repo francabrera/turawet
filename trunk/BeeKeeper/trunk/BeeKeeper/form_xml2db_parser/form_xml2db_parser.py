0a44b17a7cc3b237522d1bb58cc1d71e023c70fa
romenrg <romenrg@09e8946b-dfe9-8220-69a3-a60357482830>
2011-04-27 17:56:06 +0000

    BeeKeeper: Depurando el parser. Parseando campos y grupos. Actualizando el ejemplo "formulario.xml"

--- trunk/BeeKeeper/trunk/BeeKeeper/form_xml2db_parser/form_xml2db_parser.py
+++ trunk/BeeKeeper/trunk/BeeKeeper/form_xml2db_parser/form_xml2db_parser.py
@@ -11,6 +11,7 @@ from BeeKeeper.db_models.models import Section, Form, FormField, FieldOption, Fi
 
 
 class FormXmldbParser():
+    
     ### METHODS ###    
     def parse_text_field(self, parser, section_model):
         field_model = FormField(section=section_model)
@@ -60,10 +61,8 @@ class FormXmldbParser():
     def parse_check_field(self, parser, section_model):
         return FormField(section=section_model)
 
-    
-    def generateModels(self, xml):
-        """
-        """
+
+    def parse_generic_field(self, fields, section_model, i):
         ## Data types ##
         actionSwitch = {
             'TEXT': self.parse_text_field,
@@ -72,7 +71,48 @@ class FormXmldbParser():
             'COMBO': self.parse_combo_field,
             'CHECKBOX': self.parse_check_field
             }
+        # Parsing
+        j = 0
+        for field in fields:
+            id = field.find('id')
+            type = field.findtext("type")
+            # TO-DO: Need to check that type is valid
+            field_model = actionSwitch[type](field, section_model)
+            field_model.type = type
+            field_model.section_order = j
+            field_model.label = field.findtext('label')
+            field_model.required = field.findtext('required')
+            # TO-DO: Order inside the group (if any)
+            if field_model.required:
+                field_model.required = True
+            else:
+                field_model.required = False
+            field_model.save()
+            id.text = str(field_model.id)
+            j += 1
+
+
+    def parse_generic_group(self, groups, section_model, i):
+        # Parsing
+        j = 0
+        for group in groups:
+            id = group.find('id')
+            name = group.findtext("name")
+            required = group.findtext("required")
+            #if cond? cosa : cosa (else)
+            if required:
+                required = True
+            else:
+                required = False
+            list = group.findtext("list")
+            if list:
+                list = True
+            else:
+                list = False
+            j += 1
+                
     
+    def generateModels(self, xml):
         if xml is None:
             return None
         else:
@@ -99,23 +139,14 @@ class FormXmldbParser():
                 id.text = str(section_model.id)
                 #Section fields
                 fields = section.findall("fields/field")
-                j = 0
-                for field in fields:
-                    id = field.find('id')
-                    type = field.findtext("type")
-                    # Need to check that type is valid
-                    field_model = self.actionSwitch[type](field, section_model)
-                    field_model.order = j
-                    field_model.label = field.findtext('label')
-                    field_model.required = field.findtext('required')
-                    if field_model.required:
-                        field_model.required = True
-                    else:
-                        field_model.required = False
-                    field_model.save()
-                    id.text = str(field_model.id)
-                    j += 1
+                # Parsing the fields
+                self.parse_generic_field(fields, section_model, i)
+                # Parsing the groups
+                groups = section.findall("fields/group")
+                self.parse_generic_group(groups, section_model, i)
                 i += 1
             # Saving the XML in the form table
             form_model.xml = tostring(parser)
             form_model.save()
+            
+            return 0
