a3f42e08c9763dc02d75aa8ca61a968aa7ec1f59
nicopernas <nicopernas@09e8946b-dfe9-8220-69a3-a60357482830>
2011-06-03 16:47:13 +0000

    Cambios en el settings.py

--- trunk/BeeKeeper/trunk/BeeKeeper/settings.py
+++ trunk/BeeKeeper/trunk/BeeKeeper/settings.py
@@ -36,7 +36,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE':   'django.db.backends.oracle',
@@ -47,17 +47,18 @@ DATABASES = {
         'PORT':     '5432',
     },
 }
+"""
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#        'NAME': os.path.join(PROJECT_DIR, 'sqlite.db'),                      # Or path to database file if using sqlite3.
-#        'USER': '',                      # Not used with sqlite3.
-#        'PASSWORD': '',                  # Not used with sqlite3.
-#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_DIR, 'sqlite.db'), # Or path to database file if using sqlite3.
+        'USER': '', # Not used with sqlite3.
+        'PASSWORD': '', # Not used with sqlite3.
+        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '', # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
