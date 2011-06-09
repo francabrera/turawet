cf7420bb012d4879aa082366878251c073e6b561
franxu <franxu@09e8946b-dfe9-8220-69a3-a60357482830>
2011-06-09 14:46:56 +0000

    Cambios y correcciones en la interfaz web del modelador y preparando la conexión con el WS.

--- trunk/Seed/trunk/Seed/ws_client/beekeeperws.py
+++ trunk/Seed/trunk/Seed/ws_client/beekeeperws.py
@@ -9,8 +9,8 @@ from suds.client import Client
 logging.basicConfig(level=logging.INFO)
 logging.getLogger('suds.client').setLevel(logging.DEBUG)
 
-class WsClient(Client):
-   
-    def __init__(self):
-        url = 'http://193.145.110.236/ws_server/service.wsdl'
-        Client.__init__(self, url, cache=None)
+
+def upload_new_form(formXML):
+    url = 'http://193.145.110.236/ws_server/service.wsdl'
+    myclient = Client(url, cache=None)
+    return myclient.service.upload_new_form(formXML)
