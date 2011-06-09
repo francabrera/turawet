7bfd1391b271cf4a2901c903fa5a99f6d65e48a4
franxu <franxu@09e8946b-dfe9-8220-69a3-a60357482830>
2011-06-09 18:11:07 +0000

    Modelador conectado con el servicio web de Beekeeper (funcional), mediante String en Base64.

--- Seed/trunk/Seed/modeler/views.py
+++ Seed/trunk/Seed/modeler/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from Seed.modeler.forms import NewForm
 from Seed.ws_client.beekeeperws import upload_new_form
-from base64 import b64encode
+from base64 import urlsafe_b64encode as sb64encode
 
 #from xml.etree.ElementTree import tostring, XML
 
@@ -17,11 +17,19 @@ def showModeler(request):
         form = NewForm(request.POST)
         if form.is_valid() :
             xmlForm = form.cleaned_data['fieldList']
-            xmlFormCleaned = b64encode(xmlForm)
-            response = upload_new_form(xmlFormCleaned)
+            xmlFormCleaned = sb64encode(xmlForm)
+            errorMessage = ""
+            try:
+                response = upload_new_form(xmlFormCleaned)
+            except:
+                response = False
+                errorMessage = "Error al enviar"
+            #b64xml = sb64encode(unicode('<?xml version="1.0" encoding="UTF-8"?><form><id/><version>1</version><name>Formulario</name><author><user>turawet</user></author><sections><section><id/><name>Nombre seccion 0</name><fields><field><id /><label>Campo de texto</label><type>TEXT</type><properties/></field></fields></section></sections></form>'))
+            #upload_new_form(b64xml)
             context['prueba'] = xmlForm
             context['prueba2'] = xmlFormCleaned
             context['wsresponse'] = response
+            context['errormessage'] = errorMessage
 #        if form.is_valid() :
 #            return HttpResponseRedirect('/formList')
     else:
