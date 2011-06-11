0b626b647b55648471e3014045e216ee9521f1c4
romenrg <romenrg@09e8946b-dfe9-8220-69a3-a60357482830>
2011-06-11 16:32:13 +0000

    BeeKeeper: Administrador. "formularios", "ver_formulario", "instancias", "ver instancias". Son 4 páginas que están ya en modo prueba, con sus opciones y todo (aún sin funcionar las opciones, pero con iconos de ejemplo).

--- trunk/BeeKeeper/trunk/BeeKeeper/beekeeper/views.py
+++ trunk/BeeKeeper/trunk/BeeKeeper/beekeeper/views.py
@@ -6,7 +6,8 @@ from django.template import RequestContext
 #from numpy import zeros
 
 # Imports
-from BeeKeeper.db_models.models import Form, Section, FormField
+from BeeKeeper.db_models.models import Form, Section, FormField, Instance,\
+    InstanceField
 
 
 def showFormList (request):
@@ -18,14 +19,46 @@ def showFormList (request):
 
 def showForm (request, formid):
     
-    form = Form.objects.get(id = formid);
-    sections = Section.objects.filter(form = form)
-    i = 0
-    #form_fields = zeros(len(sections))
-    #for section in sections:
-    #    form_fields[i] = FormField.objects.filter(section=section)
-    #    i += 1
-    form_fields = FormField.objects.filter(section=sections[0])
+    form = Form.objects.filter(id = formid);
+    if form:
+        sections = Section.objects.filter(form = form)
+        i = 0
+        #form_fields = zeros(len(sections))
+        #for section in sections:
+        #    form_fields[i] = FormField.objects.filter(section=section)
+        #    i += 1
+        form_fields = FormField.objects.filter(form=form, section=sections[0])
+    else:
+        sections = None
+        form_fields = None
     
     
     return render_to_response('ver_formulario.html', {'form': form, 'sections': sections, 'form_fields': form_fields });
+
+
+def showInstanceList (request, formid):
+    
+    form = Form.objects.filter(id = formid);
+    if form:
+        instances = Instance.objects.filter(form=form);
+    
+    return render_to_response('instancias.html', {'instances': instances });
+
+
+def showInstance (request, instanceid):
+    
+    instance = Instance.objects.filter(id = instanceid);
+    if instance:
+        sections = Section.objects.filter(form = instance.form)
+        i = 0
+        #form_fields = zeros(len(sections))
+        #for section in sections:
+        #    form_fields[i] = FormField.objects.filter(section=section)
+        #    i += 1
+        instance_fields = InstanceField.objects.filter(instance=instance, section=sections[0])
+    else:
+        sections = None
+        instance_fields = None
+    
+    
+    return render_to_response('ver_instancia.html', {'instance': instance, 'sections': sections, 'instance_fields': instance_fields });
