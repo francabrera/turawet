602671bafb4ee539595ed9a05815bfb11ff6f139
romenrg <romenrg@09e8946b-dfe9-8220-69a3-a60357482830>
2011-07-03 12:23:31 +0000

    BeeKeeper: Login hecho!!!

---
+++ BeeKeeper/trunk/BeeKeeper/beekeeper/middleware.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.views import login
+from django.http import HttpResponseRedirect
+import re
+
+class SiteLogin:
+    "This middleware requires a login for every view"
+    def process_request(self, request):
+        if re.search('beekeeper', request.path) and request.path != '/beekeeper/accounts/login/' and request.user.is_anonymous():
+            if request.POST:
+                return login(request)
+            else:
+                return HttpResponseRedirect('/beekeeper/accounts/login/?next=%s' % request.path)
