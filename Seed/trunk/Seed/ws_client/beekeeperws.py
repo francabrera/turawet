9033bcec4a558999abc713b7794f60a747b6b937
nicopernas <nicopernas@09e8946b-dfe9-8220-69a3-a60357482830>
2011-11-09 21:58:25 +0000

    Subo los últimos cambios antes de hacer la migración a Git. Tengo casi terminada la traducción a XML, también las vistas de los campos de texto, fecha, radio y mapas. Todavía falta muuucho por hacer :-)

--- Seed/trunk/Seed/ws_client/beekeeperws.py
+++ Seed/trunk/Seed/ws_client/beekeeperws.py
@@ -8,11 +8,11 @@
 import logging
 from suds.client import Client
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level = logging.INFO)
 logging.getLogger('suds.client').setLevel(logging.DEBUG)
 
 
 def upload_new_form(formXML):
-    url = 'http://193.145.110.236/ws_server/service.wsdl'
-    myclient = Client(url, cache=None)
+    url = 'http://ec2-50-18-94-108.us-west-1.compute.amazonaws.com/ws_server/service.wsdl'
+    myclient = Client(url, cache = None)
     return myclient.service.upload_new_form(formXML)
